d38385eeb8ed0a37cfd4dc3c785acfe61daf55aa
lucas taranne <74668250+lulu960@users.noreply.github.com>
2022-04-21 11:03:38 +0200

    maj portfolio 21/04/2022

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,17 +9,25 @@
     <link rel="icon" href="img/logo.png">
     <meta name="description" content="Luca Taranne portfolio BTS SIO option Slam">
 </head>
+
 <body class="corp">
-<!--header-->
-<?php require 'header.php' ?>
+    <!--header-->
+    <?php require 'header.php' ?>
     <div class="Presentation">
         <h1 class="Page_division">Acceuil</h1>
     </div>
     <div class="Page_corp">
         <div class="Page_txt">
             <h1 class="Page_title_bts_sio">Présentation</h1>
+            <h2 class="Page_title_bts_sio">Qui Suis-Je ?</h2>
+            <div class="BTS_txt">
+                <p>Je m'apelle Lucas Taranne, j'ai 22 ans et je suis acctuellement en BTS SIO option SLAM (Solutions Logicielles et Applications Métiers) a IPSSI Paris.</p>
+                <p>Par la suite de ce BTS j'aimerais intégrer une licence professionnelle metier de l'informatique.</p>
+                <p>Vous pouvez télécharger mon CV en cliquant <a href="Veille/taranne-lucas-cv.pdf" target="_blank">ici</a>, et également télécharger ma fiche de compétences <a href="Veille/TARANNE_lucas-Tableau E4.pdf" target="_blank">ici</a></p>
+            </div>
         </div>
     </div>
     <?php require 'footer.php' ?>
 </body>
+
 </html>
